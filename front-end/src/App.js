1cf26b9796362c79bb2242b9f29cb19634561fd5
Amanda Hogan <ahogan@teamworks.com>
2020-02-04 11:45:03 -0500

    Remove bootstrap import

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import AddPlantForm from "./components/AddPlantForm";
 import LoginPage from './components/LoginPage';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 function App() {
