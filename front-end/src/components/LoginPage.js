1cc9f8b6c3de612ef1a3fdffd928ee6150e8ea02
Amanda Hogan <ahogan@teamworks.com>
2020-02-03 20:57:20 -0500

    Copy over login form and styled components

---
+++ front-end/src/components/LoginPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import { LoginBox, AppTitle, AppLogin } from '../styled/StyledComponents_LoginForm';
+
+const LoginPage = () => {
+  return (
+    <LoginBox>
+      <AppTitle>Water My Plants</AppTitle>
+      <AppLogin>Login to Your Account</AppLogin>
+      <LoginForm />
+      <Link className="sign_up_link" to="/">Create an account</Link>
+    </LoginBox>
+  );
+};
+
+export default LoginPage;
