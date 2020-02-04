06cdef0fdc933fbd16b8677a6f1b5e2d6fc54c4f
abqkatrina <abqkatrina@gmail.com>
2020-02-04 12:07:06 -0700

    changing to react-hook-form

--- front-end/src/components/AddPlantForm.js
+++ front-end/src/components/AddPlantForm.js
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
+import { useForm } from 'react-hook-form';
+import { ButtonStyling, AddPlantContainer, ButtonContainer } from "../styled/formStyled";
+import { InputDiv, InputLabel, ErrorMessage } from '../styled/StyledComponents_LoginForm';
 
-import {
-  ButtonStyling,
-  AddPlantContainer,
-  InputContainer,
-  RowOneStyling,
-  ButtonContainer
-} from "../styled/formStyled";
 
 const AddPlantForm = props => {
   const addNewPlant = props.addNewPlant;
@@ -26,51 +22,51 @@ const AddPlantForm = props => {
   //     addNewPlant(plant);
   //     setNewPlant({ name: "", nickname: "", species: "", h2ofrequency: "" });
   //   };
-  return (
-    <AddPlantContainer>
-      <InputContainer>
-        <form>
-          {/* <PlantStyling> */}
-          <RowOneStyling
-            id="name"
-            type="text"
-            name="name"
-            placeholder="name"
-            value={newPlant.name}
-            onChange={handleChanges}
-          />
-          <RowOneStyling
-            id="nickname"
-            type="text"
-            name="nickname"
-            placeholder="nickname"
-            value={newPlant.nickname}
-            onChange={handleChanges}
-          />
-          <RowOneStyling
-            id="species"
+
+    return (
+      <AddPlantContainer>
+        <Form>
+        <InputDiv>
+          <InputLabel htmlFor="nickname">Plant Nickname</InputLabel>
+          <input id="nickname"
+              type="text"
+              name="nickname"
+              value={newPlant.nickname}
+              onChange={handleChanges}
+              ref={register}/>
+            {errors.nickname && <ErrorMessage>{errors.nickname.message}</ErrorMessage>}
+        </InputDiv>
+            
+
+        <InputDiv>
+          <InputLabel htmlFor="species">Plant Species</InputLabel>
+          <input id="species"
             type="text"
             name="species"
-            placeholder="species"
             value={newPlant.species}
             onChange={handleChanges}
-          />
-          <RowOneStyling
-            id="h2oFrequency"
-            type="text"
-            name="h2ofrequency"
-            placeholder="h2oFrequency"
-            value={newPlant.h2oFrequency}
-            onChange={handleChanges}
-          />
+            ref={register}/>
+          {errors.species && <ErrorMessage>{errors.species.message}</ErrorMessage>}
 
-          <ButtonContainer>
-            <ButtonStyling>Add new plant</ButtonStyling>
-          </ButtonContainer>
-          {/* </PlantStyling> */}
-        </form>
-      </InputContainer>
+        </InputDiv>
+
+        <InputDiv>
+        <InputLabel htmlFor="h2oFrequency">Watering Schedule</InputLabel>
+          <input id="h2oFrequency"
+              type="text"
+              name="h2ofrequency"
+              value={newPlant.h2oFrequency}
+              onChange={handleChanges}
+              ref={register}/>
+          {errors.h2oFrequency && <ErrorMessage>{errors.h2oFrequency.message}</ErrorMessage>}
+        </InputDiv>
+            
+        <ButtonContainer>
+          <ButtonStyling>Add new plant</ButtonStyling>
+        </ButtonContainer>
+      </Form>
     </AddPlantContainer>
   );
 };
+
 export default AddPlantForm;
