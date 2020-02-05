4c5815f5b909bbb2bfced376189db49f636901f0
Amanda Hogan <ahogan@teamworks.com>
2020-02-05 13:16:13 -0500

    Add and update plant form styling consistentcies

--- front-end/src/components/AddPlantForm.js
+++ front-end/src/components/AddPlantForm.js
@@ -4,7 +4,8 @@ import {
   InputDiv,
   InputLabel,
   ErrorMessage,
-  LoginButton
+  LoginButton,
+  LoginBox
 } from "../styled/StyledComponents_LoginForm";
 
 // const ValidationSchema = Yup.object().shape({
@@ -29,53 +30,55 @@ const AddPlantForm = props => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <InputDiv>
-        <InputLabel htmlFor="nickname">Plant Nickname</InputLabel>
-        <input
-          id="nickname"
-          type="text"
-          name="nickname"
-          value={plant.nickname}
-          ref={register}
-        />
-      </InputDiv>
+    <LoginBox>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <InputDiv>
+          <InputLabel htmlFor="nickname">Plant Nickname</InputLabel>
+          <input
+            id="nickname"
+            type="text"
+            name="nickname"
+            value={plant.nickname}
+            ref={register}
+          />
+        </InputDiv>
 
-      <InputDiv>
-        <InputLabel htmlFor="species">Plant Species</InputLabel>
-        <input
-          id="species"
-          type="text"
-          name="species"
-          value={plant.species}
-          ref={register}
-        />
-        {errors.species && (
-          <ErrorMessage>{errors.species.message}</ErrorMessage>
-        )}
-      </InputDiv>
+        <InputDiv>
+          <InputLabel htmlFor="species">Plant Species</InputLabel>
+          <input
+            id="species"
+            type="text"
+            name="species"
+            value={plant.species}
+            ref={register}
+          />
+          {errors.species && (
+            <ErrorMessage>{errors.species.message}</ErrorMessage>
+          )}
+        </InputDiv>
 
-      <InputDiv>
-        <InputLabel htmlFor="h2oFrequency">Water every:</InputLabel>
-        <select
-          id="h2oFrequency"
-          type="select"
-          name="h2ofrequency"
-          value={plant.h2oFrequency}
-          ref={register}
-        >
-          <option value="1">Twice a day</option>
-          <option value="2">Once a Day</option>
-          <option value="4">Every Two Days</option>
-          <option value="14">Once a Week</option>
-        </select>
-        {errors.h2oFrequency && (
-          <ErrorMessage>{errors.h2oFrequency.message}</ErrorMessage>
-        )}
-      </InputDiv>
+        <InputDiv>
+          <InputLabel htmlFor="h2oFrequency">Water every:</InputLabel>
+          <select
+            id="h2oFrequency"
+            type="select"
+            name="h2ofrequency"
+            value={plant.h2oFrequency}
+            ref={register}
+          >
+            <option value="1">Twice a day</option>
+            <option value="2">Once a Day</option>
+            <option value="4">Every Two Days</option>
+            <option value="14">Once a Week</option>
+          </select>
+          {errors.h2oFrequency && (
+            <ErrorMessage>{errors.h2oFrequency.message}</ErrorMessage>
+          )}
+        </InputDiv>
 
-      <LoginButton>Add new plant</LoginButton>
-    </form>
+        <LoginButton>Add new plant</LoginButton>
+      </form>
+    </LoginBox>
   );
 };
 
